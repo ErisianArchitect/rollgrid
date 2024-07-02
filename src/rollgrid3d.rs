b5ff00c7b38d823c5b5e4e3eb981a3acee2a97f2
ErisianArchitect <derekforgione@gmail.com>
2024-07-01 19:32:57 -0700

    Created Bounds3D

--- src/rollgrid3d.rs
+++ src/rollgrid3d.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 
 struct TempGrid3D<T> {
     pub cells: Vec<Option<T>>,
@@ -34,4 +35,156 @@ impl<T> TempGrid3D<T> {
         let plane = self.size.0 * self.size.2;
         Some(ny as usize * plane + nz as usize * self.size.0 + nx as usize)
     }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Bounds3D {
+    pub min: (i32, i32, i32),
+    pub max: (i32, i32, i32)
+}
+
+impl Bounds3D {
+    pub fn new(min: (i32, i32, i32), max: (i32, i32, i32)) -> Self {
+        Self {
+            min,
+            max
+        }
+    }
+
+    pub fn from_bounds(a: (i32, i32, i32), b: (i32, i32, i32)) -> Self {
+        let x_min = a.0.min(b.0);
+        let y_min = a.1.min(b.1);
+        let z_min = a.2.min(b.2);
+        let x_max = a.0.max(b.0);
+        let y_max = a.1.max(b.1);
+        let z_max = a.2.max(b.2);
+        Self {
+            min: (x_min, y_min, z_min),
+            max: (x_max, y_max, z_max)
+        }
+    }
+
+    /// The size along the X axis.
+    pub fn width(&self) -> i32 {
+        self.max.0 - self.min.0
+    }
+
+    /// The size along the Y axis.
+    pub fn height(&self) -> i32 {
+        self.max.1 - self.min.1
+    }
+
+    /// The size along the Z axis.
+    pub fn depth(&self) -> i32 {
+        self.max.2 - self.min.2
+    }
+
+    pub fn volume(&self) -> i32 {
+        self.width() * self.height() * self.depth()
+    }
+
+    pub fn x_min(&self) -> i32 {
+        self.min.0
+    }
+
+    pub fn y_min(&self) -> i32 {
+        self.min.1
+    }
+    
+    pub fn z_min(&self) -> i32 {
+        self.min.2
+    }
+
+    pub fn x_max(&self) -> i32 {
+        self.max.0
+    }
+
+    pub fn y_max(&self) -> i32 {
+        self.max.1
+    }
+
+    pub fn z_max(&self) -> i32 {
+        self.max.2
+    }
+
+    // intersects would need to copy self and other anyway, so
+    // just accept copied values rather than references.
+    pub fn intersects(self, other: Bounds3D) -> bool {
+        let (ax_min, ay_min, az_min) = self.min;
+        let (ax_max, ay_max, az_max) = self.max;
+        let (bx_min, by_min, bz_min) = other.min;
+        let (bx_max, by_max, bz_max) = other.max;
+        ax_min < bx_max
+        && bx_min < ax_max
+        && ay_min < by_max
+        && by_min < ay_max
+        && az_min < bz_max
+        && bz_min < az_max
+    }
+
+    pub fn contains(self, point: (i32, i32, i32)) -> bool {
+        point.0 >= self.min.0
+        && point.1 >= self.min.1
+        && point.2 >= self.min.2
+        && point.0 < self.max.0
+        && point.1 < self.max.1
+        && point.2 < self.max.2
+    }
+
+    pub fn iter(self) -> Bounds3DIter {
+        Bounds3DIter { bounds: self, current: self.min }
+    }
+}
+
+pub struct Bounds3DIter {
+    bounds: Bounds3D,
+    current: (i32, i32, i32),
+}
+
+impl Iterator for Bounds3DIter {
+    type Item = (i32, i32, i32);
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        if self.current.2 == self.bounds.max.2 {
+            return (0, Some(0));
+        }
+        let (x, y, z) = (
+            (self.current.0 - self.bounds.min.0) as usize,
+            (self.current.1 - self.bounds.min.1) as usize,
+            (self.current.2 - self.bounds.min.2) as usize
+        );
+        let width = self.bounds.width() as usize;
+        let height = self.bounds.height() as usize;
+        let depth = self.bounds.depth() as usize;
+        let volume = self.bounds.volume() as usize;
+        let index = (y * width * depth + z * width + x);
+        (volume - index, Some(volume - index))
+    }
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current.1 == self.bounds.max.1 {
+            return None;
+        }
+        let result = self.current;
+        // inc x, then z, then y
+        // self.current = (result.0 + 1, result.1, result.2);
+        self.current = if result.0 + 1 == self.bounds.max.0 {
+            if result.2 + 1 == self.bounds.max.2 {
+                (self.bounds.min.0, result.1 + 1, self.bounds.min.2)
+            } else {
+                (self.bounds.min.0, result.1, result.2 + 1)
+            }
+        } else {
+            (result.0 + 1, result.1, result.2)
+        };
+        Some(result)
+    }
+}
+
+#[test]
+fn bounds_test() {
+    let bounds = Bounds3D::new((0, 0, 0), (3, 3, 3));
+    bounds.iter().for_each(|(x, y, z)| {
+        println!("({x},{y},{z})");
+    });
 }
