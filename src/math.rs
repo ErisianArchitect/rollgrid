b76f96bb84deb40d82e2bd37b7cf131968e46e38
ErisianArchitect <derekforgione@gmail.com>
2025-02-13 15:26:42 -0800

    Removed unused code and tests.

--- src/math.rs
+++ src/math.rs
@@ -257,19 +257,6 @@ impl RemCoord<i32> for (i32, i32, i32) {
     }
 }
 
-/// Since `i32::MAX - i32::MIN == u32::MAX`, it's possible to subtract
-/// an i32 from an i32 where the result can fit into a u32 so long as the left-hand side is greater or equal
-/// to the right-hand side.
-pub const fn checked_sub_i32_into_u32(lhs: i32, rhs: i32) -> Option<u32> {
-    if rhs > lhs {
-        return None;
-    }
-    let lhs = lhs as i64;
-    let rhs = rhs as i64;
-    let result = lhs - rhs;
-    Some(result as u32)
-}
-
 #[inline]
 pub(crate) const fn add_u32_to_i32(i32_value: i32, u32_value: u32) -> i32 {
     let conv = i32_to_u32(i32_value);
@@ -295,58 +282,4 @@ pub(crate) const fn i32_to_u32(i32_value: i32) -> u32 {
 #[inline]
 pub(crate) const fn u32_to_i32(u32_value: u32) -> i32 {
     (u32_value ^ 0x8000_0000) as i32
-}
-
-#[cfg(test)]
-mod tests {
-    use std::i32;
-
-    use super::*;
-
-    #[test]
-    fn safe_math_test() {
-        let result = checked_sub_i32_into_u32(i32::MAX, i32::MIN);
-        assert_eq!(result, Some(u32::MAX));
-        let result = add_u32_to_i32(i32::MIN, u32::MAX);
-        assert_eq!(result, i32::MAX);
-    }
-
-    #[test]
-    fn coord_math_test() {
-        let a = (1, 2);
-        let b = (3, 4);
-        assert_eq!(a.add_coord(b), (4, 6), "Add (i32, i32)");
-        assert_eq!(a.sub_coord(b), (-2, -2), "Subtract (i32, i32)");
-        assert_eq!(a.mul_coord(b), (3, 8), "Multiply (i32, i32)");
-        assert_eq!(a.div_coord(b), (0, 0), "Divide (i32, i32)");
-        assert_eq!(b.rem_coord(a), (0, 0), "Remainder (i32, i32)");
-        let a = (33, 28, 14);
-        let b = (5, 5, 5);
-        assert_eq!(a.add_coord(b), (38, 33, 19), "Add (i32, i32, i32)");
-        assert_eq!(a.sub_coord(b), (28, 23, 9), "Subtract (i32, i32, i32)");
-        assert_eq!(a.mul_coord(b), (165, 140, 70), "Multiply (i32, i32, i32)");
-        assert_eq!(a.div_coord(b), (6, 5, 2), "Divide (i32, i32, i32)");
-        assert_eq!(a.rem_coord(b), (3, 3, 4), "Remainder (i32, i32, i32)");
-        let a = (113, 144);
-        let b = 5;
-        assert_eq!(a.add_coord(b), (118, 149), "(i32, i32) + i32");
-        assert_eq!(a.sub_coord(b), (108, 139), "(i32, i32) - i32");
-        assert_eq!(a.mul_coord(b), (565, 720), "(i32, i32) * i32");
-        assert_eq!(a.div_coord(b), (22, 28), "(i32, i32) / i32");
-        assert_eq!(a.rem_coord(b), (3, 4), "(i32, i32) % i32");
-        let a = (113, 144, 246);
-        let b = 5;
-        assert_eq!(a.add_coord(b), (118, 149, 251), "(i32, i32, i32) + i32");
-        assert_eq!(a.sub_coord(b), (108, 139, 241), "(i32, i32, i32) - i32");
-        assert_eq!(a.mul_coord(b), (565, 720, 1230), "(i32, i32, i32) * i32");
-        assert_eq!(a.div_coord(b), (22, 28, 49), "(i32, i32, i32) / i32");
-        assert_eq!(a.rem_coord(b), (3, 4, 1), "(i32, i32, i32) % i32");
-    }
-
-    #[test]
-    fn convert_test() {
-        let a: (i32, i32) = (123, 456);
-        let b = a.convert::<(i64, i64)>();
-        println!("{b:?}");
-    }
 }
